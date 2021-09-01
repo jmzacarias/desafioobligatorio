84b50edb797839071ddbddb246fa0eafef59b236
jmzacarias <juanzacarias@gmail.com>
2021-08-31 23:14:58 -0300

    desafio clase 1

---
+++ js.js
@@ -0,0 +1,13 @@
+
+let nombre = prompt ('Ingrese su nombre');
+let apellido = prompt ('Ingrese su apellido');
+let nombreCompleto = nombre + ' ' + apellido;
+console.log (nombreCompleto);
+let saludo = "¡Hola " + nombreCompleto + "!";
+alert (saludo);
+
+
+let edad = prompt ('Decinos tu edad');
+let edadParseada = parseInt(edad);
+let suma = 10;
+console.log (edadParseada + suma);
